cee1356caa4d6d4aaf92eec7e755be2f9d65cce5
vchibilisco <vchibilisco@gmail.com>
2020-03-27 00:12:08 -0300

    Ejemplo FCL con Java

---
+++ Logica Difusa/ejemploFCL/src/ejemploFCL/Main.java
@@ -0,0 +1,67 @@
+package ejemploFCL;
+
+import java.util.Scanner;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+
+public class Main {
+
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		String fileName = "flc/tipper.fcl";
+		
+		FIS fis = FIS.load(fileName);
+		
+		if (fis == null) {
+			System.err.println("El archivo "+fileName+" no se pudo cargar.");
+			return;
+		}
+
+		JFuzzyChart.get().chart(fis);
+		
+		int service = setVariable(0, 9, "Servicio");
+		
+		
+		fis.setVariable("service", service);
+
+		int food = setVariable(0, 9, "Comida");
+		
+		fis.setVariable("food", food);
+
+
+		System.out.println("Procesando...");
+		fis.evaluate();
+		
+		Variable tipVariable = fis.getVariable("tip");
+
+		System.out.println("Resultado "+tipVariable.getValue());
+		
+		JFuzzyChart.get().chart(tipVariable, tipVariable.getDefuzzifier(), true);
+
+	}
+
+	
+	private static int setVariable(
+			int limit0,
+			int limit1,
+			String variable
+			)
+	{
+		int result;
+		boolean retry = true;
+		do {
+			System.out.println("Por favor, ingrese un valor entre "+limit0+" y "+limit1+" para "+variable+".");
+			result = sc.nextInt();
+						
+			if (limit0 <= result && result <= limit1) {
+				retry = false;
+			}
+
+		} while(retry);
+
+		return result;
+	}
+}
