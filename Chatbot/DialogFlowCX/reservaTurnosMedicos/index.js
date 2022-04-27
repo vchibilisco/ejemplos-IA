ac0c20c477b82f8b3d4d7a8e2d845bc73ea57d78
Vicente Chibilisco <vchibilisco@gmail.com>
2022-04-27 16:25:07 -0300

    feat: add dialogflow cx example (#2)

---
+++ Chatbot/DialogFlowCX/reservaTurnosMedicos/index.js
@@ -0,0 +1,40 @@
+const express =  require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser')
+const moment = require('moment')
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json())
+const port = process.env.ENVIRONMENT_PORT;
+
+app.post('/webhook', (req, res) => {
+  console.log(req.body.sessionInfo);
+  const tag = req.body.fulfillmentInfo.tag;
+
+  if (!!tag) {
+    switch (tag) {
+      case 'verificar_reserva':
+        const persona = req.body.sessionInfo.parameters['given-name'][0];
+        const prepaga = req.body.sessionInfo.parameters.prepaga;
+        let estado = 'invalid';
+        if (persona.toUpperCase() === 'MARIA' && ['DASUTEN', 'OSDE', 'PRENSA', 'SWISS MEDICAL'].includes(prepaga.toUpperCase())) {
+          estado = 'valid';
+        }
+        res.status(200).send({
+          sessionInfo: {
+            parameters: {
+              estado: estado
+            }
+          }
+        });
+        break;
+      default:
+        break;
+    }
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
