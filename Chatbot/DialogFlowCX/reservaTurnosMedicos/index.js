5d2afffe2e2494b1daaa69b84b4fc536a322b547
vchibilisco <vchibilisco@gmail.com>
2024-05-26 18:59:53 -0300

    feat: fix chatbot, add diabetes model

--- Chatbot/DialogFlowCX/reservaTurnosMedicos/index.js
+++ Chatbot/DialogFlowCX/reservaTurnosMedicos/index.js
@@ -2,6 +2,7 @@ const express =  require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser')
 const moment = require('moment')
+const ngrok = require('@ngrok/ngrok');
 
 dotenv.config();
 
@@ -12,16 +13,23 @@ const port = process.env.ENVIRONMENT_PORT;
 app.post('/webhook', (req, res) => {
   console.log(req.body.sessionInfo);
   const tag = req.body.fulfillmentInfo.tag;
+  console.log(req.body.sessionInfo)
 
   if (!!tag) {
     switch (tag) {
       case 'verificar_reserva':
-        const persona = req.body.sessionInfo.parameters['given-name'];
-        const prepaga = req.body.sessionInfo.parameters.prepaga;
         let estado = 'invalid';
-        if (persona.toUpperCase() === 'MARIA' && ['DASUTEN', 'OSDE', 'PRENSA', 'SWISS MEDICAL'].includes(prepaga.toUpperCase())) {
-          estado = 'valid';
+        try {
+          const persona = req.body.sessionInfo.parameters.person.name;
+          const prepaga = req.body.sessionInfo.parameters.prepaga;
+          
+          if (persona.toUpperCase() === 'MARIA' && ['DASUTEN', 'OSDE', 'PRENSA', 'SWISS MEDICAL'].includes(prepaga.toUpperCase())) {
+            estado = 'valid';
+          }
+        } catch (error) {
+          console.error(error);
         }
+        
         res.status(200).send({
           sessionInfo: {
             parameters: {
@@ -38,3 +46,5 @@ app.post('/webhook', (req, res) => {
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+ngrok.connect({ addr: 8080, authtoken_from_env: true })
+	.then(listener => console.log(`Ingress established at: ${listener.url()}`));
