719344ce9589fcc97c4138bce20944678069a99a
Vicente Chibilisco <vchibilisco@gmail.com>
2026-06-03 18:53:06 -0300

    actualizacion de repo

--- Logica Difusa/RiesgoFinanciero/src/riesgo/financiero/Main.java
+++ Logica Difusa/RiesgoFinanciero/src/riesgo/financiero/Main.java
@@ -7,16 +7,25 @@ import net.sourceforge.jFuzzyLogic.rule.Variable;
 public class Main {
 
 	public static void main(String[] args) {
-		String fileName = "flc//modelo.fcl";
+		// Construir la ruta correcta desde el directorio de trabajo actual
+		String fileName = "flc/modelo.fcl";
+		java.io.File file = new java.io.File(fileName);
+		
+		// Si no existe, intentar desde el directorio del proyecto
+		if (!file.exists()) {
+			fileName = "Logica Difusa/RiesgoFinanciero/flc/modelo.fcl";
+			file = new java.io.File(fileName);
+		}
 
 		FIS fis = FIS.load(fileName);
 
 		if (fis == null) {
 			System.err.println("El archivo " + fileName + " no se pudo cargar.");
+			System.err.println("Directorio de trabajo: " + System.getProperty("user.dir"));
 			return;
 		}
 		
-		//JFuzzyChart.get().chart(fis);
+		JFuzzyChart.get().chart(fis);
 		
 		fis.setVariable("edad", 25);
 		fis.setVariable("porcentaje_manejo", 50);
@@ -27,7 +36,7 @@ public class Main {
 
 		System.out.println("Resultado "+riesgoVariable.getValue());
 		
-		JFuzzyChart.get().chart(riesgoVariable, riesgoVariable.getDefuzzifier(), true);
+		//JFuzzyChart.get().chart(riesgoVariable, riesgoVariable.getDefuzzifier(), true);
 	}
 
 }
